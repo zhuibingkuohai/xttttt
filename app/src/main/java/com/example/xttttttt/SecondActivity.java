3874d06d12daf2278407f80d393b8bf6ebcea462
zhuibingkuohai <76981831+zhuibingkuohai@users.noreply.github.com>
2021-01-10 16:34:37 +0800

    234

--- app/src/main/java/com/example/xttttttt/SecondActivity.java
+++ app/src/main/java/com/example/xttttttt/SecondActivity.java
@@ -2,7 +2,10 @@ package com.example.xttttttt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+    }
+
+    public void qqb(View view) {
+        Toast.makeText(SecondActivity.this,"用户名和密码正确",Toast.LENGTH_SHORT).show();
+        Intent intent=new Intent(SecondActivity.this,Gqq.class);
+        startActivity(intent);
     }
 }
