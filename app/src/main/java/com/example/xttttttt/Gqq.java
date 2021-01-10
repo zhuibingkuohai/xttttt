b8c4703fc26ee91c537abf230a9830a41c5cf276
zhuibingkuohai <76981831+zhuibingkuohai@users.noreply.github.com>
2021-01-10 15:54:08 +0800

    6565989

---
+++ app/src/main/java/com/example/xttttttt/Gqq.java
@@ -0,0 +1,14 @@
+package com.example.xttttttt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Gqq extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gqq);
+    }
+}
